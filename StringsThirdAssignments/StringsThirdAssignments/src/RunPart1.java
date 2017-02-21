660eec5c30bddb723e030c782fbc40ac862e61a7
Siyuan Zheng <raymondzsy@outlook.com>
2017-02-21 00:26:56 -0500

    Uploading programs folder

---
+++ StringsThirdAssignments/StringsThirdAssignments/src/RunPart1.java
@@ -0,0 +1,9 @@
+
+public class RunPart1 {
+
+	public static void main(String[] args) {
+		Part1 part1 = new Part1();
+		part1.testGetAllGenes();
+	}
+
+}
---
+++ StringsThirdAssignments/StringsThirdAssignments/src/RunPart2.java
@@ -0,0 +1,10 @@
+
+public class RunPart2 {
+
+	public static void main(String[] args) {
+		Part2 part2 = new Part2();
+		part2.testCgRatio();
+		part2.testCountCTG();
+	}
+
+}
