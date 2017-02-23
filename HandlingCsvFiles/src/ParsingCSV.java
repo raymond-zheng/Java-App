5fdd4f82408ead9bb53e88e8589d0078615b0bb4
Siyuan Zheng <raymondzsy@outlook.com>
2017-02-23 03:25:53 -0500

    parsing .csv

---
+++ HandlingCsvFiles/src/ParsingCSV.java
@@ -0,0 +1,74 @@
+/**
+ * Created by Siyuan Zheng on 2017-02-23.
+ */
+import org.apache.commons.csv.*;
+import edu.duke.*;
+
+public class ParsingCSV
+{
+    public void tester(){
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        //each time calling a new method needs resetting the parser
+        //parser = fr.getCSVParser();
+        System.out.print("The information about Germany :" + "\n");
+        System.out.print(countryInfo(parser,"Germany"));
+        System.out.print("\n");
+
+        parser = fr.getCSVParser();
+        System.out.print("The countries that both export coffee and tea: " + "\n");
+        listExportersTwoProducts(parser,"coffee","tea");
+        System.out.print("\n");
+
+        parser = fr.getCSVParser();
+        System.out.print("The number of countries that export motor vehicles" + "\n");
+        System.out.print(numberOfExporters(parser,"motor vehicles"));
+        System.out.print("\n\n");
+
+        System.out.print("The countries that has larger exports than $720,000,000" + "\n");
+        parser = fr.getCSVParser();
+        bigExporters(parser,"$720,000,000");
+    }
+
+    //part2
+    public String countryInfo(CSVParser parser, String country){
+        for(CSVRecord record : parser){
+            String countryName = record.get("Country");
+            if(countryName.contains(country)){
+                String exports = record.get("Exports");
+                String value = record.get("Value (dollars)");
+                return countryName + ": " + exports + ": " + value + "\n";
+            }
+        }
+        return "";
+    }
+
+    //part3
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2){
+        for(CSVRecord record: parser) {
+            if (record.get("Exports").contains(exportItem1) && record.get("Exports").contains(exportItem2)) {
+                System.out.print(record.get("Country") + "\n");
+            }
+        }
+    }
+
+    //part4
+    public int numberOfExporters(CSVParser parser, String exportItem){
+        int count = 0;
+        for(CSVRecord record: parser){
+            if(record.get("Exports").contains(exportItem)){
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    //part5
+    public void bigExporters(CSVParser parser, String amount){
+        for(CSVRecord record: parser){
+            if(record.get("Value (dollars)").length() > amount.length()){
+                System.out.print(record.get("Country") + " " + record.get("Value (dollars)") + "\n");
+            }
+        }
+    }
+}
---
+++ HandlingCsvFiles/src/RunParsingCSV.java
@@ -0,0 +1,11 @@
+/**
+ * Created by raymondz on 2017-02-23.
+ */
+
+//use psvm to generate main method quickly
+public class RunParsingCSV {
+    public static void main(String[] args) {
+        ParsingCSV parsingCSV = new ParsingCSV();
+        parsingCSV.tester();
+    }
+}
